3bf0c91778772c541d350455fa76295b0f2d6754
RabbitProgram <rabbitprogram.2018@gmail.com>
2024-04-11 21:13:52 +0900

    feat: オリジナルに修正

--- pages_test/src/App.tsx
+++ pages_test/src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import jobProgrammer from "./job_programmer.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={jobProgrammer}
+          alt="jobProgrammer"
+          style={{ width: "20vw" }}
+        />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Cloudflare Pages のテストページです
+          <br />
+          リポジトリは
+          <a
+            className="App-link"
+            href="https://github.com/RabbitProgram/cloudflare_test/tree/main/pages_test"
+          >
+            こちら
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
